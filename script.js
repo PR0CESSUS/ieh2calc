9d12fdfd06268a772adb22d4350a2361e49a06d9
Processus <processus@outlook.com>
2023-04-02 21:20:30 +0200

    version: 0.0.7 - added anvil calc

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 function load() {
+    if (localStorage.getItem('setting-currentTab')) {
+      if (localStorage.getItem('setting-currentTab') == 'slime-bank') {
+        document.getElementById('slime-bank-calculator').style.display = 'block';
+        document.getElementById('anvil-calculator').style.display = 'none';
+        document.getElementById('slime-bank').style.color = '#f2e009';
+        document.getElementById('lab-calculator').style.display = 'none';    
+      }
+      else if (localStorage.getItem('setting-currentTab') == 'lab') {
+        document.getElementById('slime-bank-calculator').style.display = 'none';
+        document.getElementById('anvil-calculator').style.display = 'none';
+        document.getElementById('lab-calculator').style.display = 'block';
+        document.getElementById('lab').style.color = '#f2e009';
+      }
+      else if (localStorage.getItem('setting-currentTab') == 'anvil') {
+        document.getElementById('slime-bank-calculator').style.display = 'none';
+        document.getElementById('lab-calculator').style.display = 'none';
+        document.getElementById('anvil-calculator').style.display = 'block';
+        document.getElementById('anvil').style.color = '#f2e009';
+      }
 
+    } else {
+      localStorage.setItem('setting-currentTab', 'slime-bank');
+      document.getElementById('anvil-calculator').style.display = 'none';
+      document.getElementById('slime-bank').style.color = '#f2e009';
+      document.getElementById('lab-calculator').style.display = 'none';   
+    }
+    // loading anvil calc from localStorage
+    if (localStorage.getItem('anvil-nitro-speed')) {document.getElementById('anvil-nitro-speed').value = localStorage.getItem('anvil-nitro-speed');} 
+    else {localStorage.setItem('anvil-nitro-speed', 1)}
+    if (localStorage.getItem('anvil-fox-dungeon-1-time')) {
+      document.getElementById('anvil-fox-dungeon-1-time').value = localStorage.getItem('anvil-fox-dungeon-1-time');
+      document.getElementById('anvil-fox-dungeon-2-time').value = localStorage.getItem('anvil-fox-dungeon-2-time');
+      document.getElementById('anvil-fox-dungeon-3-time').value = localStorage.getItem('anvil-fox-dungeon-3-time');
+      document.getElementById('anvil-fox-dungeon-4-time').value = localStorage.getItem('anvil-fox-dungeon-4-time');
+      document.getElementById('anvil-fox-dungeon-5-time').value = localStorage.getItem('anvil-fox-dungeon-5-time');
+      document.getElementById('anvil-bat-dungeon-1-time').value = localStorage.getItem('anvil-bat-dungeon-1-time');
+      document.getElementById('anvil-bat-dungeon-2-time').value = localStorage.getItem('anvil-bat-dungeon-2-time');
+      document.getElementById('anvil-bat-dungeon-3-time').value = localStorage.getItem('anvil-bat-dungeon-3-time');
+      document.getElementById('anvil-bat-dungeon-4-time').value = localStorage.getItem('anvil-bat-dungeon-4-time');
+      document.getElementById('anvil-bat-dungeon-5-time').value = localStorage.getItem('anvil-bat-dungeon-5-time');
+      
+      
+      calc_anvil();
+    } else {calc_anvil();}
 
     if (localStorage.getItem('slime-bank-research-stone-level')) {
       document.getElementById('slime-bank-research-stone-level').value = localStorage.getItem('slime-bank-research-stone-level');
@@ -33,7 +76,7 @@ function load() {
   // 2.55e+19      
     
     // document.getElementById('test').innerHTML = convert2('120.33k');
-
+    // calc_anvil();
     Slime_Bank_Gold_Cap_Total();    
     Slime_Bank_Intrest();
     Slime_Bank_Optimiser();
@@ -41,6 +84,43 @@ function load() {
     // console.log(key);
     // console.log(localStorage.getItem(key));
 }
+
+function calc_anvil_save(input) {
+  localStorage.setItem(input, document.getElementById(input).value);
+  calc_anvil();
+}
+
+
+function calc_anvil() {
+  const list = ['anvil-fox-dungeon-1','anvil-fox-dungeon-2','anvil-fox-dungeon-3','anvil-fox-dungeon-4','anvil-fox-dungeon-5',
+  'anvil-bat-dungeon-1','anvil-bat-dungeon-2','anvil-bat-dungeon-3','anvil-bat-dungeon-4','anvil-bat-dungeon-5'];
+  var nitro = localStorage.getItem('anvil-nitro-speed');
+
+  list.forEach(element => {
+    // console.log(element);
+    value = element + '-value';
+    time = element + '-time';
+    reward = document.getElementById(element + '-reward').innerHTML;
+    // console.log(reward);    
+    document.getElementById(value).innerHTML = Math.round(3600 / document.getElementById(time).value * reward * nitro);
+  });
+  // list.forEach(myFunction);
+  // if (localStorage.getItem('anvil-fox-dungeon-1-time') && document.getElementById('anvil-fox-dungeon-1-time').value != localStorage.getItem('anvil-fox-dungeon-1-time')) {
+  //   document.getElementById('anvil-fox-dungeon-1-time').value = localStorage.getItem('anvil-fox-dungeon-1-time');
+  // } 
+  // else {localStorage.setItem('anvil-fox-dungeon-1-time', document.getElementById('anvil-fox-dungeon-1-time').value);}
+   
+  //  document.getElementById('anvil-fox-dungeon-1-value').innerHTML = 3600 / document.getElementById('anvil-fox-dungeon-1-time').value;
+}
+
+
+
+function currentTab(input) {
+  localStorage.setItem('setting-currentTab', input);
+}
+
+
+
 
 function restart() {
   window.localStorage.clear();
@@ -51,7 +131,7 @@ function convert(input) {
   input = parseFloat(input);
   // console.log(input);
   var output = 0;
-  if (input <= 1000) {output = Math.round(input);}
+  if (input <= 10000) {output = Math.round(input);}
     else if (input < 1000000) {output = (input / 1000).toFixed(2) + 'K';}
     else if (input < 1000000000) {output = (input / 1000000).toFixed(2) + 'M';}
     else if (input < 1000000000000) {output = (input / 1000000000).toFixed(2) + 'B';}
@@ -150,7 +230,7 @@ function Upgrade_Slime_Bank_Gold_Cap_1(input) {
       sum += buffer;
       i++;
   }
-    document.getElementById('slime-bank-upgrade-slime-coin-cap-2-value').innerHTML = sum+100+'%';
+    document.getElementById('slime-bank-upgrade-slime-coin-cap-2-value').innerHTML = convert(sum+100)+'%';
     localStorage.setItem('slime-bank-upgrade-slime-coin-cap-2-level', input);
     localStorage.setItem('slime-bank-upgrade-slime-coin-cap-2-value', sum + 100);
     // return base + base * input + sum;
