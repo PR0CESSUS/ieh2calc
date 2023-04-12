6a347dec1484e107efba8dcda1942fd8200e70a2
Processus <processus@outlook.com>
2023-04-12 18:08:53 +0200

    hot fix for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "."),
     globalObject: "this",
     library: "ieh2calc",
     libraryTarget: "umd",
